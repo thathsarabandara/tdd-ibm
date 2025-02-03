43ca776763f7fcd1774865459cd7082c05098cc1
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-02-04 02:36:08 +0530

    factories and fakes

--- labs/05_factories_and_fakes/tests/factories.py
+++ labs/05_factories_and_fakes/tests/factories.py
@@ -20,3 +20,9 @@ class AccountFactory(factory.Factory):
         model = Account
 
     # Add attributes here...
+    id=factory.sequence(lambda n:n)
+    name=factory.faker("name")
+    name=factory.faker("email")
+    phone_number=factory.faker("phone_number")
+    disabled=FuzzyChoice(choices=[True,False])
+    date_joined= FuzzyDate(date(2008,1,1))
