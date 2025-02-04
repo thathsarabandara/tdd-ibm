93cf40f806e2b782c7fbce0458bf910d4a2b70ff
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-02-04 13:30:13 +0530

    generate steps

--- labs/11_generating_steps/features/steps/web_steps.py
+++ labs/11_generating_steps/features/steps/web_steps.py
@@ -8,3 +8,40 @@ For information on Waiting until elements are present in the HTML see:
 """
 
 # Replace this comment with your web steps...
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+
+@given(u'I am on the "Home Page"')
+def step_impl(context):
+    context.response = context.driver.get(context.base_url)
+
+@when(u'I set the "Category" to "dog"')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'pet_category')
+    element.clear()
+    element.send_keys('dog')
+
+@when(u'I click the "Search" button')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'search-btn')
+    element.click()
+
+@then(u'I should see the message "Success"')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'flash_message')
+    assert "Success" in element.text
+
+@then(u'I should see "Fido" in the results')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert "Fido" in element.text
+
+@then(u'I should not see "Kitty" in the results')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert "Kitty" not in element.text
+
+@then(u'I should not see "Leo" in the results')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert "Leo" not in element.text
