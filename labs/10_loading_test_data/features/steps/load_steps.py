55f6dbd98be6ecdddabd8bf0ed10185c070a5be4
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-02-04 12:31:19 +0530

    wrote the test data

--- labs/10_loading_test_data/features/steps/load_steps.py
+++ labs/10_loading_test_data/features/steps/load_steps.py
@@ -10,4 +10,22 @@ import requests
 from behave import given
 
 # Load data here
+@given('the following pets')
+def step_impl(context):
+    """Refresh all Pets in the database"""
+    responese = requests.get(f"{context.base_url}/pets")
+    assert responese.status_code == 200
+    for pet in responese.json():
+        responese = requests.delete(f"{context.base_url}/pets/{pet['id']}")
+        assert responese.status_code == 204
 
+    for row in context.table:
+        payload  = {
+            "name" : row['name'],
+            "category" : row['category'],
+            "available" : row['available'] in ['True', 'true', '1'],
+            "gender" : row['gender'],
+            "birthday" : row['birthday']
+        }
+        responese = requests.post(f"{context.base_url}/pets", json=payload)
+        assert responese.status_code == 201 
